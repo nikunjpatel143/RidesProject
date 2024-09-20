e2de4ded6b948ce9329fda272ecbffa4753fa313
Nikunj <nikunjpatel143@gmail.com>
2024-09-20 10:08:56 -0400

    additional info 1

--- app/src/main/java/com/example/rides/views/WelcomeScreen.kt
+++ app/src/main/java/com/example/rides/views/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.rides.views
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -19,7 +21,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,7 +32,7 @@ import androidx.compose.ui.unit.sp
 fun welcomeScreen(onClick: (numbersOfVehicle: String) -> Unit) {
 
     var textValue by remember { mutableStateOf("") }
-
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -53,15 +57,22 @@ fun welcomeScreen(onClick: (numbersOfVehicle: String) -> Unit) {
                         Color.LightGray
                     )
                 )
-            }
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
 
-        Button(modifier = Modifier.height(75.dp).width(150.dp),
+        Button(modifier = Modifier
+            .height(75.dp)
+            .width(150.dp),
             onClick = {
-                if(textValue.isEmpty()){
-                    textValue = "10"
+                if (textValue.isEmpty()) {
+                    Toast.makeText(context, "Enter valid number", Toast.LENGTH_LONG).show()
+                } else if (textValue.toInt() > 100) {
+                    Toast.makeText(context, "The number should be less then 100", Toast.LENGTH_LONG)
+                        .show()
+                } else {
+                    onClick(textValue)
                 }
-                onClick(textValue)
             }
         ) {
             Text(text = "Click Me", fontSize = 20.sp)
